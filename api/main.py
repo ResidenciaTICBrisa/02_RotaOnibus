088f5e504df6a4cb3332058b673ec5a8dfeaec94
LucasLopesFrazao <lucaslopesfrazao2003@gmail.com>
2023-09-11 09:30:59 -0300

    :sparkles: Adicionando API inicial

--- api/main.py
+++ api/main.py
@@ -1,20 +1,57 @@
-from fastapi import FastAPI
-import random
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get('/')
+class Coordinates(BaseModel):
+    lat_origin: float
+    lon_origin: float
+    lat_destination: float
+    lon_destination: float
+
+
+@app.get('/', tags=['Root'], summary='Verifica se o servidor está rodando')
 async def root():
-    return {'example': 'This is an example', 'data': 999}
+    return {'Servidor rodando!'}
 
-@app.get('/random')
-async def get_random():
-    rn:int = random.randint(0, 100)
-    return {'number': rn, 'limit': 100}
+@app.post('/paradas/',
+          tags=['Paradas'],
+          summary='Calcula e retorna as paradas ao longo do caminho',
+          description='''Este endpoint aceita coordenadas de origem e destino e retorna as paradas necessárias para a realização do trajeto.  
+          - `lat_origin`: Latitude da origem (entre -90 e 90)  
+          - `lon_origin`: Longitude da origem (entre -180 e 180)  
+          - `lat_destination`: Latitude do destino (entre -90 e 90)  
+          - `lon_destination`: Longitude do destino (entre -180 e 180)'''
+          )
+async def get_coordinates(coordinates: Coordinates):
+    if not (-90 <= coordinates.lat_origin <= 90):
+        raise HTTPException(
+            status_code=400, detail="Latitude de origem inválida. Deve ser entre -90 e 90.")
 
-@app.get('/random/{limit}')
-async def get_random(limit: int):
-    rn:int = random.randint(0, limit)
-    return {'number': rn, 'limit': limit}
+    if not (-180 <= coordinates.lon_origin <= 180):
+        raise HTTPException(
+            status_code=400, detail="Longitude de origem inválida. Deve ser entre -180 e 180.")
 
-# uvicorn main:app --reload
+    if not (-90 <= coordinates.lat_destination <= 90):
+        raise HTTPException(
+            status_code=400, detail="Latitude de destino inválida. Deve ser entre -90 e 90.")
+
+    if not (-180 <= coordinates.lon_destination <= 180):
+        raise HTTPException(
+            status_code=400, detail="Longitude de destino inválida. Deve ser entre -180 e 180.")
+
+    # Simulação de paradas como uma lista de dicionários
+    paradas = [
+        {'nome': 'Parada 1', 'coordenadas': [40.712776, -74.005974]},
+        {'nome': 'Parada 2', 'coordenadas': [34.052235, -118.243683]},
+        {'nome': 'Parada 3', 'coordenadas': [41.878113, -87.629799]},
+        {'nome': 'Parada 4', 'coordenadas': [42.878113, -88.629799]},
+        {'nome': 'Parada 5', 'coordenadas': [43.878113, -89.629799]},
+        {'nome': 'Parada 6', 'coordenadas': [44.878113, -90.629799]},
+
+        # Adicione mais paradas conforme necessário
+    ]
+
+    return {'origin': [coordinates.lat_origin, coordinates.lon_origin],
+            'destination': [coordinates.lat_destination, coordinates.lon_destination],
+            'paradas': paradas}
