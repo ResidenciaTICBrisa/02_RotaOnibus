768599e6f9cc1f2d62ba3c66135af6926c684b89
LucasLopesFrazao <lucaslopesfrazao2003@gmail.com>
2023-09-25 12:51:55 -0300

    :sparkle: Adicionando API_Baldeação

--- api/main.py
+++ api/main.py
@@ -1,26 +1,98 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+import osmnx as ox
+import networkx as nx
+import pickle
+import math
 
 app = FastAPI()
+
+# Definição do modelo Pydantic para entrada
+
 
 class Coordinates(BaseModel):
     lat_origin: float
     lon_origin: float
-    lat_destination: float
-    lon_destination: float
+
+
+# Carregando o grafo das paradas de ônibus
+with open('bus_stops_graph.pkl', 'rb') as f:
+    bus_stops_graph = pickle.load(f)
+
+# Função para calcular a distância haversine
+
+
+def haversine(coord1, coord2):
+    lat1, lon1 = coord1
+    lat2, lon2 = coord2
+    R = 6371000  # raio da Terra em metros
+    phi_1 = math.radians(lat1)
+    phi_2 = math.radians(lat2)
+    delta_phi = math.radians(lat2 - lat1)
+    delta_lambda = math.radians(lon2 - lon1)
+    a = math.sin(delta_phi / 2.0) ** 2 + math.cos(phi_1) * \
+        math.cos(phi_2) * math.sin(delta_lambda / 2.0) ** 2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    meters = R * c
+    return meters
+
+# Função para encontrar a parada de ônibus mais próxima
+
+
+def find_nearest_stop(coord, graph):
+    nearest_stop = None
+    nearest_distance = float('inf')
+    for node, attributes in graph.nodes(data=True):
+        stop_coord = (attributes['lat'], attributes['lon'])
+        distance = haversine(coord, stop_coord)
+        if distance < nearest_distance:
+            nearest_distance = distance
+            nearest_stop = node
+    return graph.nodes[nearest_stop]['lat'], graph.nodes[nearest_stop]['lon']
+
+# Função para calcular e desenhar a rota de caminhada
+
+
+def calculate_route(start_point, end_point, graph):
+    nearest_start = ox.distance.nearest_nodes(
+        graph, X=[start_point[1]], Y=[start_point[0]])[0]
+    nearest_end = ox.distance.nearest_nodes(
+        graph, X=[end_point[1]], Y=[end_point[0]])[0]
+    route = nx.shortest_path(graph, nearest_start,
+                             nearest_end, weight='length', method='dijkstra')
+    route_coords = [(graph.nodes[node]['y'], graph.nodes[node]['x'])
+                    for node in route]
+    return route_coords
 
 
 @app.get('/', tags=['Root'], summary='Verifica se o servidor está rodando')
 async def root():
     return {'Servidor rodando!'}
 
+
+@app.post('/calculate_route', tags=['Route'], summary='Calcula a rota até a parada de ônibus mais próxima')
+async def get_route(coordinates: Coordinates):
+    start_point = (coordinates.lat_origin, coordinates.lon_origin)
+
+    # Encontrar a parada de ônibus mais próxima usando o grafo das paradas de ônibus
+    end_point = find_nearest_stop(start_point, bus_stops_graph)
+
+    # Baixar o mapa da área de interesse para cálculo da rota de caminhada
+    G = ox.graph_from_point(start_point, dist=2000, network_type='walk')
+
+    # Calcular a rota
+    route_coords = calculate_route(start_point, end_point, G)
+
+    return {"baldeacao": route_coords}
+
+
 @app.post('/paradas/',
           tags=['Paradas'],
           summary='Calcula e retorna as paradas ao longo do caminho',
-          description='''Este endpoint aceita coordenadas de origem e destino e retorna as paradas necessárias para a realização do trajeto.  
-          - `lat_origin`: Latitude da origem (entre -90 e 90)  
-          - `lon_origin`: Longitude da origem (entre -180 e 180)  
-          - `lat_destination`: Latitude do destino (entre -90 e 90)  
+          description='''Este endpoint aceita coordenadas de origem e destino e retorna as paradas necessárias para a realização do trajeto.
+          - `lat_origin`: Latitude da origem (entre -90 e 90)
+          - `lon_origin`: Longitude da origem (entre -180 e 180)
+          - `lat_destination`: Latitude do destino (entre -90 e 90)
           - `lon_destination`: Longitude do destino (entre -180 e 180)'''
           )
 async def get_coordinates(coordinates: Coordinates):
---
+++ api/teste.py
@@ -0,0 +1,30 @@
+import requests
+import folium
+
+# URL do endpoint do FastAPI
+API_URL = "http://127.0.0.1:8000/calculate_route"
+
+# Coordenadas de origem (Você pode alterá-las conforme necessário)
+lat_origin = -15.819223
+lon_origin = -47.874458 
+
+# Fazendo a requisição para a API
+response = requests.post(
+    API_URL, json={"lat_origin": lat_origin, "lon_origin": lon_origin})
+
+# Verificar se a requisição foi bem-sucedida
+if response.status_code == 200:
+    data = response.json()
+    route_coords = data['baldeacao']
+
+    # Criando o mapa com folium centrado na origem
+    m = folium.Map(location=[lat_origin, lon_origin], zoom_start=15)
+
+    # Adicionando o caminho no mapa
+    folium.PolyLine(route_coords, color="blue", weight=2.5).add_to(m)
+
+    # Salva o mapa em um arquivo HTML e abre no navegador
+    m.save("./route_map.html")
+    print("Mapa salvo como 'route_map.html'. Abra este arquivo em um navegador para ver a rota.")
+else:
+    print(f"Erro ao chamar a API: {response.text}")
