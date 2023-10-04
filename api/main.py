cf4e65fbde6c7aa822f0f81a31da2ff592695283
LucasLopesFrazao <lucaslopesfrazao2003@gmail.com>
2023-10-04 10:26:49 -0300

    :sparkles: Desenho da rota

--- api/main.py
+++ api/main.py
@@ -1,9 +1,9 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from pydantic import BaseModel
 import osmnx as ox
 import networkx as nx
 import pickle
-import math
+from typing import List
 
 app = FastAPI()
 
@@ -12,37 +12,18 @@ class Coordinates(BaseModel):
     lat_origin: float
     lon_origin: float
 
+class BusStop(BaseModel):
+    lat: float
+    lon: float
+
+# Modelo Pydantic atualizado
+class InputData(BaseModel):
+    current_location: BusStop
+    paradas: List[BusStop]
 
 # Carregando o grafo das paradas de ônibus
 with open('grafoDirecional.grafo', 'rb') as f:
     bus_stops_graph = pickle.load(f)
-
-# Função para calcular a distância haversine
-def haversine(coord1, coord2):
-    lat1, lon1 = coord1
-    lat2, lon2 = coord2
-    R = 6371000  # raio da Terra em metros
-    phi_1 = math.radians(lat1)
-    phi_2 = math.radians(lat2)
-    delta_phi = math.radians(lat2 - lat1)
-    delta_lambda = math.radians(lon2 - lon1)
-    a = math.sin(delta_phi / 2.0) ** 2 + math.cos(phi_1) * \
-        math.cos(phi_2) * math.sin(delta_lambda / 2.0) ** 2
-    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
-    meters = R * c
-    return meters
-
-# Função para encontrar a parada de ônibus mais próxima
-def find_nearest_stop(coord, graph):
-    nearest_stop = None
-    nearest_distance = float('inf')
-    for node, attributes in graph.nodes(data=True):
-        stop_coord = (attributes['coords'].y, attributes['coords'].x)
-        distance = haversine(coord, stop_coord)
-        if distance < nearest_distance:
-            nearest_distance = distance
-            nearest_stop = node
-    return graph.nodes[nearest_stop]['coords'].y, graph.nodes[nearest_stop]['coords'].x
 
 # Função para calcular e desenhar a rota de caminhada
 def calculate_route(start_point, end_point, graph):
@@ -56,64 +37,44 @@ def calculate_route(start_point, end_point, graph):
                     for node in route]
     return route_coords
 
-
 @app.get('/', tags=['Root'], summary='Verifica se o servidor está rodando')
 async def root():
     return {'Servidor rodando!'}
 
-
-@app.post('/calculate_route', tags=['Route'], summary='Calcula a rota até a parada de ônibus mais próxima')
-async def get_route(coordinates: Coordinates):
-    start_point = (coordinates.lat_origin, coordinates.lon_origin)
-
-    # Encontrar a parada de ônibus mais próxima usando o grafo das paradas de ônibus
-    end_point = find_nearest_stop(start_point, bus_stops_graph)
+@app.post('/calculate_route', tags=['Route'], summary='Calcula as rotas de baldeação e veicular')
+async def get_route(data: InputData):
+    # 1. Rota de Baldeação (Caminhada)
+    start_point = (data.current_location.lat, data.current_location.lon)
+    # Pegue a primeira parada de ônibus da lista
+    first_bus_stop = data.paradas[0]
+    end_point = (first_bus_stop.lat, first_bus_stop.lon)
 
     # Baixar o mapa da área de interesse para cálculo da rota de caminhada
     G = ox.graph_from_point(start_point, dist=2000, network_type='walk')
 
-    # Calcular a rota
-    route_coords = calculate_route(start_point, end_point, G)
+    # Calcular a rota de baldeação
+    baldeacao_coords = calculate_route(start_point, end_point, G)
 
-    return {"baldeacao": route_coords}
+    # 2. Rota Veicular
+    G_DF = ox.load_graphml("private_false_distrito_federal.graphml")
 
+    # Começando da primeira parada de ônibus e seguindo para as outras paradas
+    all_stops = data.paradas
 
-@app.post('/paradas/',
-          tags=['Paradas'],
-          summary='Calcula e retorna as paradas ao longo do caminho',
-          description='''Este endpoint aceita coordenadas de origem e destino e retorna as paradas necessárias para a realização do trajeto.
-          - `lat_origin`: Latitude da origem (entre -90 e 90)
-          - `lon_origin`: Longitude da origem (entre -180 e 180)
-          - `lat_destination`: Latitude do destino (entre -90 e 90)
-          - `lon_destination`: Longitude do destino (entre -180 e 180)'''
-          )
-async def get_coordinates(coordinates: Coordinates):
-    if not (-90 <= coordinates.lat_origin <= 90):
-        raise HTTPException(
-            status_code=400, detail="Latitude de origem inválida. Deve ser entre -90 e 90.")
+    nearest_nodes = [ox.distance.nearest_nodes(
+        G_DF, stop.lon, stop.lat) for stop in all_stops]
 
-    if not (-180 <= coordinates.lon_origin <= 180):
-        raise HTTPException(
-            status_code=400, detail="Longitude de origem inválida. Deve ser entre -180 e 180.")
+    all_routes = []
+    for i in range(len(nearest_nodes) - 1):
+        route_segment = nx.shortest_path(
+            G_DF, nearest_nodes[i], nearest_nodes[i + 1], weight='length')
+        all_routes.extend(route_segment[:-1])
+    all_routes.append(nearest_nodes[-1])
 
-    if not (-90 <= coordinates.lat_destination <= 90):
-        raise HTTPException(
-            status_code=400, detail="Latitude de destino inválida. Deve ser entre -90 e 90.")
+    veicular_coords = [(G_DF.nodes[node]['y'], G_DF.nodes[node]['x'])
+                       for node in all_routes]
 
-    if not (-180 <= coordinates.lon_destination <= 180):
-        raise HTTPException(
-            status_code=400, detail="Longitude de destino inválida. Deve ser entre -180 e 180.")
-
-    # Simulação de paradas como uma lista de dicionários
-    paradas = [
-        {'nome': 'Parada 1', 'coordenadas': [40.712776, -74.005974]},
-        {'nome': 'Parada 2', 'coordenadas': [34.052235, -118.243683]},
-        {'nome': 'Parada 3', 'coordenadas': [41.878113, -87.629799]},
-        {'nome': 'Parada 4', 'coordenadas': [42.878113, -88.629799]},
-        {'nome': 'Parada 5', 'coordenadas': [43.878113, -89.629799]},
-        {'nome': 'Parada 6', 'coordenadas': [44.878113, -90.629799]},
-    ]
-
-    return {'origin': [coordinates.lat_origin, coordinates.lon_origin],
-            'destination': [coordinates.lat_destination, coordinates.lon_destination],
-            'paradas': paradas}
+    return {
+        "baldeacao": baldeacao_coords,
+        "rota_veicular": veicular_coords
+    }
